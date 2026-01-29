2184f0b2bb93a5952a616779b7e3fea7d5d3d0f9
mapas48a <jonathansanchezq1@gmail.com>
2026-01-28 22:53:52 -0400

    feat: Implement a new tech stack section by creating dedicated Astro components for icons and data, and refactoring the main tech stack component.

---
+++ src/lib/techData.tsx
@@ -0,0 +1,94 @@
+import Reacticon from "@/icons/reacticon.astro";
+import Tailwind from "@/icons/tailwind.astro";
+import Nextjs from "@/icons/nextjs.astro";
+import Astroicon from "@/icons/astroicon.astro";
+import Nodejs from "@/icons/nodejs.astro";
+import Lua from "@/icons/lua.astro";
+import Sqlite from "@/icons/sqlite.astro";
+import Gemini from "@/icons/gemini.astro";
+import Chatgpt from "@/icons/chatgpt.astro";
+import Blender from "@/icons/blender.astro";
+import Canva from "@/icons/canva.astro";
+import Rust from "@/icons/rust.astro";
+import Cplusplus from "@/icons/cplusplus.astro";
+import Cchart from "@/icons/cchart.astro";
+import Python from "@/icons/python.astro";
+import Java from "@/icons/java.astro";
+import Github from "@/icons/github.astro";
+
+export const techDataIKnow = [
+    {
+        name: "React",
+        Icon: Reacticon,
+    },
+    {
+        name: "Nextjs",
+        Icon: Nextjs,
+    },
+    {
+        name: "Nodejs",
+        Icon: Nodejs,
+    },
+    {
+        name: "Tailwind",
+        Icon: Tailwind,
+    },
+    {
+        name: "Astro",
+        Icon: Astroicon,
+    },
+    {
+        name: "Lua",
+        Icon: Lua,
+    },
+    {
+        name: "Git",
+        Icon: Github,
+    },
+    {
+        name: "Sqlite",
+        Icon: Sqlite,
+    },
+];
+
+export const techDataISecondary = [
+    {
+        name: "Gemini",
+        Icon: Gemini,
+    },
+    {
+        name: "ChatGPT",
+        Icon: Chatgpt,
+    },
+    {
+        name: "Blender",
+        Icon: Blender,
+    },
+    {
+        name: "Canva",
+        Icon: Canva,
+    },
+];
+
+export const techDataIWantLearn = [
+    {
+        name: "Rust",
+        Icon: Rust,
+    },
+    {
+        name: "C++",
+        Icon: Cplusplus,
+    },
+    {
+        name: "C#",
+        Icon: Cchart,
+    },
+    {
+        name: "Python",
+        Icon: Python,
+    },
+    {
+        name: "Java",
+        Icon: Java,
+    },
+];
